15794e969d482b018a382182ae79acfe0ac774ac
Oussama <Hassouneoussama8@gmail.com>
2026-03-11 02:50:19 +0000

    add PDF extraction

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from supabase import create_client, Client
+import os
+from pydantic import BaseModel, ConfigDict
+from ocr import extract_TEXT_from_pdf
+
+load_dotenv()
 
 app = FastAPI()
 app.add_middleware(
@@ -9,8 +17,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get('/')
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+
+
+@app.get('/companies')
 def read_root():
-    return{'message':"hello world"}
+    result = supabase.table('company').select('*').execute()
+    return{'companies':result.data}
+
+class Company(BaseModel):
+    name: str
+    tax_number: str
+    address: str
+    
+@app.post('/add-company/')
+async def addCompany(company :Company ):
+    data =  supabase.table('company').insert({
+        "name": company.name,
+        'tax_number':company.tax_number,
+        'address': company.address
+    }).execute()
+    return data.data
+
+
+@app.post("/uploadPdf")
+async def uploadPDF(file: UploadFile):
+    if not file.filename.endswith('.pdf'):
+        raise HTTPException(status_code=400, detail='File must be a PDF')
+    pdf = await file.read()
+    try:
+        result = extract_TEXT_from_pdf(pdf)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Something went wrong")
+
+    return {'text':result}
+
+
 
 
---
+++ backend/ocr.py
@@ -0,0 +1,30 @@
+import pymupdf
+import pytesseract
+from PIL import Image
+import io
+from fastapi import HTTPException
+
+def extract_TEXT_from_pdf(pdf):
+    try:
+        if isinstance(pdf ,bytes):
+            doc = pymupdf.open(stream=pdf, filetype='pdf')
+        else:
+            doc = pymupdf.open(pdf)
+    except Exception as e:
+        raise ValueError(f"Could not open pdf: {e}")
+    pages= []
+    for page in doc: 
+        text = page.get_text()
+        if len(text.strip()) == 0:
+            raise ValueError('PDF page is empty')
+        if len(text.strip())< 50:
+            mat = pymupdf.Matrix(2, 2)
+            pix = page.get_pixmap(matrix=mat)
+            img = Image.open(io.BytesIO(pix.tobytes("png")))
+            text = pytesseract.image_to_string(img)
+            pages.append(text)
+        else:
+            pages.append(text)
+        
+    return pages 
+
