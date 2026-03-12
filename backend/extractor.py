89414fb9a909c6034ce04e837e09d95b9b317613
Oussama <Hassouneoussama8@gmail.com>
2026-03-12 02:16:28 +0000

    add invoice confirmation endpoint

--- backend/extractor.py
+++ backend/extractor.py
@@ -8,7 +8,7 @@ def convert_text_to_json_Ai(result):
     try:
         response = client.responses.create(
             model="gpt-4.1-mini",
-            input=f'You are an invoice data extractor. Extract the following fields from each invoice in the text below and return a JSON array where each item represents one invoice: [  "invoice_number": "", "date": "", "amount_ht": "", "vat": "", "amount_ttc": ""  ] Return ONLY the JSON array, no extra text. Invoice text: {result}')
+            input=f'You are an invoice data extractor. Extract the following fields from each invoice in the text below and return a JSON array where each item represents one invoice: [  "invoice_number": "", "date": return in yyyy-mm-dd format "", "amount_ht": "", "vat": "", "amount_ttc": ""  ] Return ONLY the JSON array, no extra text. Invoice text: {result}')
         text = response.output[0].content[0].text
         invoices = json.loads(text)
         return invoices
--- backend/main.py
+++ backend/main.py
@@ -57,10 +57,60 @@ async def uploadPDF(file: UploadFile):
     try:
         invoices = convert_text_to_json_Ai(result)
     except ValueError as e:
-            raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
     except Exception:
         raise HTTPException(status_code=500, detail='Something went wrong')
     return {'text':invoices}
+
+
+class Invoice(BaseModel):
+    invoice_number : str
+    date : str
+    amount_ht : float
+    vat: float
+    amount_ttc : float
+
+class FullInvoice(BaseModel):
+    company_id: str
+    invoices: list[Invoice]
+    
+@app.post("/confirm-invoices")
+async def confirmation(dataInvoice : FullInvoice):
+    try:
+        company_name = supabase.table('company').select('name').eq('id',dataInvoice.company_id ).execute()
+        if not company_name.data: 
+            raise HTTPException(status_code=404, detail="Company not found")
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException (status_code=500, detail=f"Something went wrong: {str(e)}")
+    records = []
+    try:
+        if not dataInvoice.invoices:
+            raise HTTPException(status_code=400, detail='No Invoices Provided')
+        for invoice in  dataInvoice.invoices:
+            records.append({
+                'company_id': dataInvoice.company_id,
+                'supplier_name': company_name.data[0]['name'],
+                "invoice_number": invoice.invoice_number,
+                "invoice_date": invoice.date, 
+                "amount_ht":invoice.amount_ht ,
+                "vat": invoice.vat,
+                "amount_ttc": invoice.amount_ttc 
+            })
+        data = supabase.table('invoices').insert(records).execute()
+    except HTTPException:
+        raise
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail="Not only information were provided")
+    except TypeError as e:
+        raise HTTPException(status_code=400, detail='Enter the right information')
+    except Exception:
+        raise HTTPException(status_code=500 , detail='Something went wrong')
+    
+
+    return data.data
+
 
 
 
